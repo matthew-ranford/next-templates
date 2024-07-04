89a7cac35847d2df27bda1cc984eec5a550ded4c
Matthew Ranford <matt.ranford16@gmail.com>
2024-07-04 18:07:32 +1200

    Setup of Navbar component, add navlinks and simple map to start

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import Link from 'next/link'
+
+export default function Navbar() {
+  const navLinks = [
+    { href: '/', text: 'Home' },
+    { href: 'our-story', text: 'Our Story' },
+    { href: 'work', text: 'Work' },
+    { href: 'services', text: 'Services' },
+    { href: 'contact', text: 'Contact' },
+  ]
+
+  return (
+    <>
+      <nav className="flex justify-between pt-4 text-2xl">
+        <h1 className="ps-7">Website Name</h1>
+        <ul className="flex gap-5 pe-7">
+          {navLinks.map((link, index) => (
+            <Link href={link.href} key={index}>
+              <li className="list-none">{link.text}</li>
+            </Link>
+          ))}
+        </ul>
+      </nav>
+    </>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
+import Navbar from './components/Navbar'
+
 export default function Home() {
   return (
     <>
-      <>
-        <h1>Page template test</h1>
-      </>
+      <Navbar />
     </>
   )
 }
